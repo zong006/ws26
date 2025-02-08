6a90313eeeed9845d5d287ad2247e6bd213b9726
zong006 <ongzhicong@gmail.com>
2025-02-08 15:38:39 +0800

    first commit

--- src/main/java/vttp/ws/ws26/repo/MongoConstants.java
+++ src/main/java/vttp/ws/ws26/repo/MongoConstants.java
@@ -1,6 +1,6 @@
 package vttp.ws.ws26.repo;
 
 public class MongoConstants {
-    public static final String MONGO_BGG_C_NAME = "game";
+    public static final String MONGO_BGG_C_NAME = "games";
     public static final String  MONGO_BGG_RANKING = "ranking";
 }
